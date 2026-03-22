de962ec7c5959c41d15c001cc1d6862e89faf15d
JazSemStorz <lan.podlesnik@gmail.com>
2026-03-22 23:01:26 +0100

    Main function redy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+bool readNumbers(vector<unsigned char>& vec, const char* filename) {};
+
+
+int main(int argc, char* argv[]) {
+    vector<unsigned char> A;
+    if (argc != 2) {
+        cout << "Usage: vaja2 <input_file>" << endl;
+        return 1;
+    }
+    if (!readNumbers(A, argv[1])) {
+        cout << "Error: Cannot open input file" << endl;
+        return 1;
+    }
+
     return 0;
 }
