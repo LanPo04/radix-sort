bda37215aa02a6e19bf7f273e7f8c57808cbc485
JazSemStorz <lan.podlesnik@gmail.com>
2026-03-22 23:19:08 +0100

    Add readNumbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+bool readNumbers(vector<unsigned char>& vec, const char* filename) {
+    ifstream input(filename);
+    int temp;
+    while (input >> temp) {
+        if (temp >= 0 && temp <= 255) {
+            vec.push_back(static_cast<unsigned char>(temp));
+        }
+    }
+    input.close();
+    return true;
+}
+
+
+int main(int argc, char* argv[]) {
+    vector<unsigned char> A;
+    if (argc != 2) {
+        cout << "Usage: vaja2 <input_file>" << endl;
+        return 1;
+    }
+    if (!readNumbers(A, argv[1])) {
+        cout << "Error: Cannot open input file" << endl;
+        return 1;
+    }
     return 0;
 }
